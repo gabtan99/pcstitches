c46d8c5099b350fb4ee46a9a62357b35ddee112e
Gabriel Tan <gabtan99@gmail.com>
2019-02-07 22:22:12 +0800

    removes print statements

--- src/designchallenge1/DesignChallenge1.java
+++ src/designchallenge1/DesignChallenge1.java
@@ -14,10 +14,7 @@ public class DesignChallenge1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //testefsfeesfse
-//HIHIx
-        //shshs
+
         CalendarProgram cp = new CalendarProgram();
 
     }
