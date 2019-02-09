716d7f317b7e6926d0a9f5e101ac7deb546fc148
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-02-09 11:09:37 +0800

    added events to the FBViewObserver

--- src/designchallenge1/FBViewObserver.java
+++ src/designchallenge1/FBViewObserver.java
@@ -2,6 +2,7 @@ package designchallenge1;
 
 import facebook.FBView;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class FBViewObserver extends ViewObserver{
@@ -17,6 +18,14 @@ public class FBViewObserver extends ViewObserver{
         ArrayList<Event> events = subject.getEvents();
         if (events.size() > 0){
             view = new FBView();
+            for (Event e: events){
+                String name = e.getName();
+                int month = e.getStartMonth();
+                int day = e.getStartMonth();
+                int year = e.getStartYear();
+                Color color = e.getColor();
+                view.showNewEvent(name, month, day, year, color);
+            }
         }
 
     }
